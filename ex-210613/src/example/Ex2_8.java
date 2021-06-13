1b4c1a1468407ccef29da1443ae7c037f49aeed7
LeeGiCheol <asdqwek200@naver.com>
2021-06-13 10:27:49 +0900

    연습문제 변수

---
+++ ex-210613/src/example/Ex2_8.java
@@ -0,0 +1,28 @@
+package example;
+
+public class Ex2_8 {
+
+	public static void main(String[] args) {
+		int x = 1;
+		int y = 2;
+		int z = 3;
+		
+		
+		int temp = x;
+		x = y;
+		y = z;
+		z = temp;
+		
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+		
+		char ch = 'A';
+		
+		
+		System.out.println(Float.MAX_VALUE);
+		System.out.println(Long.MAX_VALUE);
+		
+	}
+	
+}
